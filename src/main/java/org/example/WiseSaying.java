67d35a4b08a5ca9fd63b9cbbfe9e45217ac9bfca
JaeBeom <80519614+Genie16-github@users.noreply.github.com>
2023-03-06 15:30:21 +0900

    목록 기능 êµ테스트 (#6)

---
+++ src/main/java/org/example/WiseSaying.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class WiseSaying {
+    public long id;
+    public String content;
+    public String author;
+
+    public WiseSaying(long id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+}
